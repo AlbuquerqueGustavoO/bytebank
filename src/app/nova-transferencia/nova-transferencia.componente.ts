fece0709c2b59a443c6b0e30a83136e7b9aa4daf
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-12-07 15:42:35 -0300

    nova transferencia e appcomponente

--- src/app/nova-transferencia/nova-transferencia.componente.ts
+++ src/app/nova-transferencia/nova-transferencia.componente.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+
 
 @Component({
     selector:'app-nova-transferencia',
@@ -8,7 +9,20 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent{
 
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor: number;
+    destino: number;
+
     transferir(){
-        console.log('Solicitado nova transferencia')
+        // console.log('Solicitado nova transferencia');
+        const valorEmitir = {valor: this.valor, destino: this.destino}
+        this.aoTransferir.emit(valorEmitir);
+        this.limparCampos();
+    }
+
+    limparCampos(){
+        this.valor = 0;
+        this.destino = 0;
     }
 }
